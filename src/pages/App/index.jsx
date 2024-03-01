1cfb10597e90c888bbb8d0086f17ff65387688fc
Andres Vergara <pipevergara28@hotmail.com>
2024-03-01 01:16:38 -0500

    corrigiendo aun

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -11,7 +11,7 @@ import { MyOrderByID } from "../MyOrderByID";
 import "./App.css";
 import { NavBar } from "../../components/NavBar";
 import { CheckoutSideMenu } from "../../components/CheckoutDetail";
-import { CartProvider } from "../../context";
+import { CartProvider } from "../../context/index.jsx";
 
 const AppRoutes = () => {
   let routes = useRoutes([
